03a5c9aaea7bcc3a68a535d4a38912fe971ded4f
Gu Yingbo <guyingbo@papayamobile.com>
2017-08-28 16:39:53 +0800

    aiofluent.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+import os.path
+import re
+VERSION_RE = re.compile(r'''__version__ = ['"]([0-9.]+)['"]''')
+BASE_PATH = os.path.dirname(__file__)
+
+
+with open(os.path.join(BASE_PATH, 'aiofluent.py')) as f:
+    try:
+        version = VERSION_RE.search(f.read()).group(1)
+    except IndexError:
+        raise RuntimeError('Unable to determine version.')
+
+
+setup(
+    name='aiofluent',
+    description='A useful asynchronous library bases on aiobotocore',
+    license='MIT',
+    version=version,
+    author='Yingbo Gu',
+    author_email='tensiongyb@gmail.com',
+    maintainer='Yingbo Gu',
+    maintainer_email='tensiongyb@gmail.com',
+    url='https://github.com/guyingbo/aiofluent',
+    py_modules=['aiofluent'],
+    python_requires='>=3.6',
+    install_requires=[
+        'msgpack-python',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Intended Audience :: Developers',
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+)
