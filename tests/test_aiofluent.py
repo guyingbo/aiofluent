8ba8808ade661003738d31fcb5b3256f4b514129
Gu Yingbo <guyingbo@papayamobile.com>
2017-08-30 10:28:35 +0800

    add a test case

---
+++ tests/test_aiofluent.py
@@ -0,0 +1,9 @@
+import asyncio
+from aiofluent import FluentSender
+loop = asyncio.get_event_loop()
+
+
+def test_fluent():
+    sender = FluentSender()
+    loop.run_until_complete(sender.close())
+    loop.close()
