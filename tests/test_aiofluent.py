b36f8622c3ad055f4ea76108de48f723f7f5a8a5
Gu Yingbo <guyingbo@papayamobile.com>
2019-01-22 18:30:45 +0800

    test

--- tests/test_aiofluent.py
+++ tests/test_aiofluent.py
@@ -5,10 +5,10 @@ from aiofluent import FluentSender
 
 def test_fluent():
     loop = asyncio.get_event_loop()
-    sender = FluentSender("tag", host=None)
     dic = {"name": "test"}
 
     async def go():
+        sender = FluentSender("tag", host=None)
         await sender.emit("label", dic)
         sender.server_sock.send(b"haha")
         data = sender.server_sock.recv(1024)
@@ -24,6 +24,10 @@ def test_fluent():
         assert label == "tag.label2"
         assert obj == dic
 
+        await sender.close()
+
+        sender = FluentSender("tag")
+        await sender.emit("label", dic)
         await sender.close()
 
     loop.run_until_complete(go())
